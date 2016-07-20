6bdaf9172a966807259d33c446a49cb9098c9027
majunm <747673016@qq.com>
2016-07-20 16:50:26 +0800

    add log

--- src/main/jni/Hello.cpp
+++ src/main/jni/Hello.cpp
@@ -1,7 +1,9 @@
 #include <jni.h>
 #include "july_appn_July.h"
+#include "log.h"
 JNIEXPORT jstring JNICALL Java_july_appn_July_hello
         (JNIEnv *env, jclass thiz){
+    LOGD("jni=%s","hello word");
     return env->NewStringUTF("hello lady.....");
 }
 
